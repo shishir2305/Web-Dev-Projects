d476678043fd16e6874f77e23b06bb60792b939d
Shishir Jha <shishir230501@gmail.com>
2023-09-11 11:47:06 +0530

    Ticket functionalities added

--- 02_Jira_Ticket_Management_System_Clone/script.js
+++ 02_Jira_Ticket_Management_System_Clone/script.js
@@ -1,8 +1,54 @@
 const addBtn = document.querySelector(".add-btn");
+const removeBtn = document.querySelector(".remove-btn");
 const modalCont = document.querySelector(".modal-cont");
 const mainCont = document.querySelector(".main-cont");
 const textareaCont = document.querySelector(".textarea-cont");
+const allPriorityColors = document.querySelectorAll(".priority-color");
+const lockElem = document.querySelector(".ticket-lock");
+const colorFilter = document.querySelectorAll(".color");
+
+const colors = ["lightpink", "lightblue", "lightgreen", "black"];
+let modalPriorityColor = colors[colors.length - 1];
 let addFlag = false;
+let removeFlag = false;
+const lockClass = "fa-lock";
+const unlockClass = "fa-lock-open";
+let ticketsArr = [];
+
+allPriorityColors.forEach((colorElem, idx) => {
+  colorElem.addEventListener("click", (e) => {
+    allPriorityColors.forEach((colorElem) => {
+      colorElem.classList.remove("border");
+    });
+    colorElem.classList.add("border");
+    modalPriorityColor = colorElem.classList[1];
+  });
+});
+
+colorFilter.forEach((colorElem) => {
+  colorElem.addEventListener("click", (e) => {
+    let currColor = colorElem.classList[1];
+    let allTickets = document.querySelectorAll(".ticket-cont");
+    allTickets.forEach((ticket) => {
+      let ticketColor = ticket.querySelector(".ticket-color");
+      let ticketColorClass = ticketColor.classList[1];
+      if (currColor == ticketColorClass) {
+        ticket.style.display = "block";
+      } else {
+        ticket.style.display = "none";
+      }
+    });
+  });
+});
+
+colorFilter.forEach((colorElemt) => {
+  colorElemt.addEventListener("dblclick", (e) => {
+    let allTickets = document.querySelectorAll(".ticket-cont");
+    allTickets.forEach((ticket) => {
+      ticket.style.display = "block";
+    });
+  });
+});
 
 addBtn.addEventListener("click", () => {
   addFlag = !addFlag;
@@ -16,22 +62,69 @@ addBtn.addEventListener("click", () => {
 
 modalCont.addEventListener("keydown", (e) => {
   if (e.key == "Shift") {
-    createTicket();
+    createTicket(modalPriorityColor, shortid(), textareaCont.value);
     modalCont.style.display = "none";
     addFlag = false;
     textareaCont.value = "";
   }
 });
 
-function createTicket() {
+removeBtn.addEventListener("click", () => {
+  removeFlag = !removeFlag;
+});
+
+function createTicket(ticketColor, ticketID, ticketTask) {
   let ticketCont = document.createElement("div");
   ticketCont.setAttribute("class", "ticket-cont");
   ticketCont.innerHTML = `
-    <div class="ticket-color"></div>
-    <div class="ticket-id">#example</div>
-    <div class="task-area">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-    aperiam! Id fuga, itaque odio illum eveniet nihil reiciendis
-    reprehenderit quisquam.</div>
+    <div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id">#${ticketID}</div>
+    <div class="task-area">${ticketTask}</div>
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+      </div>
   `;
+  ticketsArr.push({
+    ticketColor,
+    ticketID,
+    ticketTask,
+  });
   mainCont.appendChild(ticketCont);
+  handleRemoval(ticketCont);
+  handleLock(ticketCont);
+  handleColor(ticketCont);
+}
+
+function handleRemoval(ticket) {
+  if (removeFlag) {
+    ticket.remove();
+  }
+}
+
+function handleLock(ticket) {
+  let ticketLockElem = ticket.querySelector(".ticket-lock");
+  let ticketTaskArea = ticket.querySelector(".task-area");
+  let ticketLock = ticketLockElem.children[0];
+  ticketLock.addEventListener("click", () => {
+    if (ticketLock.classList.contains(lockClass)) {
+      ticketLock.classList.remove(lockClass);
+      ticketLock.classList.add(unlockClass);
+      ticketTaskArea.setAttribute("contenteditable", "true");
+    } else {
+      ticketLock.classList.remove(unlockClass);
+      ticketLock.classList.add(lockClass);
+      ticketTaskArea.setAttribute("contenteditable", "false");
+    }
+  });
+}
+
+function handleColor(ticket) {
+  let ticketColor = ticket.querySelector(".ticket-color");
+  ticketColor.addEventListener("click", () => {
+    let currColor = ticketColor.classList[1];
+    let idx = colors.indexOf(currColor);
+    let newIdx = (idx + 1) % colors.length;
+    ticketColor.classList.remove(currColor);
+    ticketColor.classList.add(colors[newIdx]);
+  });
 }
